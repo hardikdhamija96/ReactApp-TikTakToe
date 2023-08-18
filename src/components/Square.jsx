b6d9dd69f436b654cf28b28aaf30de3c773a89c2
hardik <hardikdhamija96@gmail.com>
2023-08-18 11:58:59 +0530

    code files

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Square = (props) => {
+  return (
+    <div 
+    onClick={props.onClick}
+    style={{border:"5px solid black", height:"100px", width:"100%", display:"flex", justifyContent:"center", alignItems:"center", fontSize:"35px",margin:"2px"}} className='square'>
+        <h5>{props.value}</h5>
+    </div>
+  )
+}
+
+export default Square
